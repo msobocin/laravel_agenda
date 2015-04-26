1b7f2b1d28315a22438404ee9c604085c105f186
msobocin <msobocin@jaumebalmes.net>
2015-04-26 16:20:01 +0200

    BD v2

---
+++ database/migrations/2015_04_26_140037_create_citas_and_contactos_tables.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitasAndContactosTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('citas', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('slug')->default('');
+            $table->datetime('data');
+            $table->string('titol')->default('');
+            $table->string('lloc')->default('');
+            $table->text('descripcio')->default('');
+            $table->timestamps();
+        });
+        Schema::create('contactos', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('nom')->default('');
+            $table->string('slug')->default('');
+            $table->string('email')->default('');
+            $table->string('tlf')->default('');
+            $table->timestamps();
+        });
+
+        Schema::create('cita_contacto', function(Blueprint $table)
+        {
+            $table->integer('cita_id')->unsigned()->index();
+            $table->foreign('cita_id')->references('id')->on('citas')->onDelete('cascade');
+
+            $table->integer('contacto_id')->unsigned()->index();
+            $table->foreign('contacto_id')->references('id')->on('contactos')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('citas');
+        Schema::drop('contactos');
+        Schema::drop('cita_contacto');
+    }
+}
